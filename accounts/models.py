fd44e9ffedd53fca9d657ff801ef3077f4723a25
Ramadhan Sulthon Alfanie <jannahy313@gmail.com>
2025-12-15 08:18:40 +0700

    adding length phone

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20)  # 628xxx
+
+    def __str__(self):
+        return self.user.email
 # Create your models here.
