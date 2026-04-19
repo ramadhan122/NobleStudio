6fdba6e0a482b356f796523f36c1d7730a2f6bfd
Ramadhan Sulthon Alfanie <jannahy313@gmail.com>
2026-04-19 10:15:21 +0700

    Setup PostgreSQL

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -13,9 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from django.contrib.auth import authenticate, login
+import dj_database_url
+import os
 
-
-
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
